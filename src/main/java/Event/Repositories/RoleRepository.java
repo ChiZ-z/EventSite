a86778e4650ebd76b5dd3a24fec3920fb9f3e00c
ChiZ-z <ivan.forever91@gmail.com>
2018-10-31 01:29:19 +0300

    Change mysql structure

---
+++ src/main/java/Event/Repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package Event.Repositories;
+
+
+import Event.Users.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
