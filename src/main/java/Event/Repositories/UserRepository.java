86ab8da409db916139f5e26b5599071741735a71
ChiZ-z <ivan.forever91@gmail.com>
2018-11-03 13:04:22 +0300

    Deleted all

--- src/main/java/Event/Repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package Event.Repositories;
-
-import Event.Users.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
